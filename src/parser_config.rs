34d48e769218832c67d99db04c05db187931feb1
Lucas L. S. Haine <lucaslshaine@gmail.com>
2026-04-28 23:32:38 -0300

    test: test ParserConfig methods

--- src/parser_config.rs
+++ src/parser_config.rs
@@ -80,3 +80,147 @@ impl ParserConfig {
         self.configs.values()
     }
 }
+
+#[cfg(test)]
+mod test_parser_config {
+    use super::*;
+    use std::sync::LazyLock;
+    use crate::parser_config;
+
+    static PARSER_CONFIG_CELL: LazyLock<ParserConfig> = LazyLock::new(|| {
+        parser_config! {
+            "strict" => Flag,
+            "output" => Text,
+            "loglevel" => Count,
+            "tasks" => List,
+            "u" => Text,
+            "U" => Alias { target: "u" },
+            "g" => Text,
+            "G" => Alias { target: "g" },
+            "user" => Alias { target: "u" },
+            "group" => Alias { target: "g" },
+        }.unwrap()
+    });
+
+    #[test]
+    fn test_contains_key() {
+        let config: &ParserConfig = &PARSER_CONFIG_CELL;
+        assert!(config.contains_key("strict"));
+        assert!(!config.contains_key("quiet"));
+    }
+
+    #[test]
+    fn test_get() {
+        let config: &ParserConfig = &PARSER_CONFIG_CELL;
+        let expected = Some(ConfigEntry::Text { default: None });
+        assert_eq!(config.get("output"), expected.as_ref());
+    }
+
+    #[test]
+    fn test_get_key_value() {
+        let config: &ParserConfig = &PARSER_CONFIG_CELL;
+        let expected = ("output", &ConfigEntry::Text { default: None });
+        assert_eq!(config.get_key_value("output"), Some(expected));
+    }
+
+    #[test]
+    fn test_is_empty() {
+        let config: &ParserConfig = &PARSER_CONFIG_CELL;
+        let empty_config = ParserConfig::new(ConfigEntries::Map(HashMap::new()))
+            .unwrap();
+        assert!(!config.is_empty());
+        assert!(empty_config.is_empty());
+    }
+
+    #[test]
+    fn test_iter() {
+        let config: &ParserConfig = &PARSER_CONFIG_CELL;
+
+        let expected: HashMap<String, ConfigEntry> = HashMap::from([
+            ("strict".to_string(), ConfigEntry::Flag),
+            ("output".to_string(), ConfigEntry::Text { default: None }),
+            ("loglevel".to_string(), ConfigEntry::Count),
+            ("tasks".to_string(), ConfigEntry::List { sep: None }),
+            ("u".to_string(), ConfigEntry::Text { default: None }),
+            ("U".to_string(), ConfigEntry::Alias { target: "u".to_string() }),
+            ("g".to_string(), ConfigEntry::Text { default: None }),
+            ("G".to_string(), ConfigEntry::Alias { target: "g".to_string() }),
+            ("user".to_string(), ConfigEntry::Alias { target: "u".to_string() }),
+            ("group".to_string(), ConfigEntry::Alias { target: "g".to_string() }),
+        ]);
+
+        for (option, entry) in config.iter() {
+            assert_eq!(Some(entry), expected.get(option));
+        }
+    }
+
+    #[test]
+    fn test_keys() {
+        let config: &ParserConfig = &PARSER_CONFIG_CELL;
+
+        for key in config.keys() {
+            assert!(config.contains_key(key));
+        }
+    }
+
+    #[test]
+    fn test_len() {
+        let config: &ParserConfig = &PARSER_CONFIG_CELL;
+
+        assert_eq!(config.len(), 10);
+    }
+
+    #[test]
+    fn test_values() {
+        let config: &ParserConfig = &PARSER_CONFIG_CELL;
+        let values: Vec<ConfigEntry> = Vec::from([
+            ConfigEntry::Flag,
+            ConfigEntry::Text { default: None },
+            ConfigEntry::Count,
+            ConfigEntry::List { sep: None },
+            ConfigEntry::Text { default: None },
+            ConfigEntry::Alias { target: "u".to_string() },
+            ConfigEntry::Text { default: None },
+            ConfigEntry::Alias { target: "g".to_string() },
+            ConfigEntry::Alias { target: "u".to_string() },
+            ConfigEntry::Alias { target: "g".to_string() },
+        ]);
+
+        for value in config.values() {
+            assert!(values.contains(value));
+        }
+    }
+
+    #[test]
+    fn return_error_on_missing_alias_target() {
+        let config_result = parser_config! {
+            "tasks" => List,
+            "T" => Alias { target: "Tasks" },
+        };
+        let error = config_result.unwrap_err();
+        let expected = ArgotError::AliasTargetNotFound {
+            option: "T".to_string(),
+            target: "Tasks".to_string(),
+        };
+
+        assert_eq!(error, expected);
+        assert_eq!(format!("{}", error), format!("{}", expected));
+    }
+
+    #[test]
+    fn return_error_on_invalid_alias_target() {
+        let config_result = parser_config! {
+            "tasks" => List,
+            "Tasks" => Alias { target: "tasks" },
+            "T" => Alias { target: "Tasks" },
+        };
+        let error = config_result.unwrap_err();
+        let expected = ArgotError::InvalidAliasTarget {
+            option: "T".to_string(),
+            target: "Tasks".to_string(),
+        };
+
+        assert_eq!(error, expected);
+        assert_eq!(format!("{}", error), format!("{}", expected));
+    }
+}
